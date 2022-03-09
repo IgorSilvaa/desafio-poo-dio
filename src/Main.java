ef9572cceb47ceb915ac5387a0a289eb53ffbbb1
Igor Silva <igorsilvabr99@gmail.com>
2022-03-09 13:32:42 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -20,9 +22,49 @@ public class Main {
 		mentoria.setDescricao("Descrição mentoria java");
 		mentoria.setData(LocalDate.now());
 		
-		System.out.println(curso1);
+		/*System.out.println(curso1);
 		System.out.println(curso2);
-		System.out.println(mentoria);
+		System.out.println(mentoria);*/
+		
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java Developer");
+		bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
+		
+		Dev devIgor = new Dev();
+		devIgor.setNome("Igor");
+		devIgor.inscreverBootcamp(bootcamp);
+		System.out.println("Conteudos Inscritos Igor: " + devIgor.getConteudosInscritos());
+		
+		devIgor.progredir();
+		devIgor.progredir();
+		
+		System.out.println("-");
+		System.out.println("Conteudos Inscritos Igor: " + devIgor.getConteudosInscritos());
+		System.out.println("Conteudos Concluídos Igor: " + devIgor.getConteudosConcluidos());
+		System.out.println("XP " + devIgor.calcularTotalXp());
+		
+		System.out.println("-----------------");
+		
+		Dev devThais = new Dev();
+		devThais.setNome("Thais");
+		devThais.inscreverBootcamp(bootcamp);
+		System.out.println("Conteudos Inscritos Thais: " + devThais.getConteudosInscritos());
+
+		devThais.progredir();
+		devThais.progredir();
+		devThais.progredir();
+		
+		System.out.println("-");
+		System.out.println("Conteudos Inscritos Thais: " + devThais.getConteudosInscritos());
+		System.out.println("Conteudos Concluídos Thais: " + devThais.getConteudosConcluidos());
+		System.out.println("XP " + devThais.calcularTotalXp());
+		
+		
+		
+		
 		
 	}
 
